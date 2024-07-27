afadd9eb052797486d763da4a7ab0fb8fbb686a7
Nishanthnr7 <127499581+Nishanthnr7@users.noreply.github.com>
2024-07-27 10:54:43 +0530

    Header Section Created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,15 @@
 import React from "react";
 import "./App.css";
+import Header from "./components/layouts/Header";
+import Home from "./components/layouts/Home";
 
 export default function App() {
-  return <div></div>;
+  return (
+  <div className="App">
+    <Header/>
+    <div className="container container-fluid">
+      <Home/>
+    </div>
+  </div>
+  );
 }
---
+++ frontend/src/components/layouts/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import CountRestaurant from './CountRestaurant'
+
+export default function Home() {
+  return (
+    <div>
+      <CountRestaurant/>
+    </div>
+  )
+}
