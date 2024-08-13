59b71e76ef2c8f7b2b516ded3a5ea19bc3990783
Nishanthnr7 <127499581+Nishanthnr7@users.noreply.github.com>
2024-08-13 20:48:23 +0530

    Created userActions & userReducer

--- frontend/src/components/layouts/FoodItem.jsx
+++ frontend/src/components/layouts/FoodItem.jsx
@@ -1,43 +1,48 @@
-import React from 'react';
+import React from "react";
+import { useAlert } from "react-alert";
 import { FaIndianRupeeSign } from "react-icons/fa6";
 
+export default function FoodItem({ fooditem }) {
 
-export default function FoodItem({Fooditem}) {
+  const alert = useAlert
+
   return (
     <div className="col-sm-12 col-md-6 col-lg-3 my-3">
-        <div className="card p-3 rounded">
-            <img src={Fooditem.images[0].url} 
-            alt="Pizza" className="card-img-top mx-auto" 
-            />
-            {/*Heading and discripton*/}
-            <div className="card-body d-flex flex-column">
-                <h5 className="card-title">Veg Corn Pizza</h5>
-                <p className="fooditem_des">
-                    Crunchy and Cheesy veg loaded Pizza served with happiness.
-                </p>
-                <p className="card-text">
-                  <FaIndianRupeeSign />
-                  180
-                  <br />
-                </p>
-                <button 
-                  type="button"
-                  id="cart_btn"
-                  className="btn btn-primary d-inline ml-4"> Add to Cart</button>
-                <br />
-                <p>
-                    Status:{"  "} 
-                    <span id="stock_status"
-                    className={10>5 ? "greenColor" : "redColor"}>
-                    {10>5? "In Stock" : "Out of Stock"}
-                    
-                    </span>
-                    </p>
-            </div>
+      <div className="card p-3 rounded">
+        <img
+          src={fooditem.images[0].url}
+          alt={fooditem.name}
+          className="card-img-top mx-auto"
+        />
+        {/*Heading and discripton*/}
+        <div className="card-body d-flex flex-column">
+          <h5 className="card-title">{fooditem.name}</h5>
+          <p className="fooditem_des">{fooditem.description}</p>
+          <p className="card-text">
+            <FaIndianRupeeSign />
+            {fooditem.price}
+            <br />
+          </p>
+          <button
+            type="button"
+            id="cart_btn"
+            className="btn btn-primary d-inline ml-4"
+          >
+            {" "}
+            Add to Cart
+          </button>
+          <br />
+          <p>
+            Status:{"  "}
+            <span
+              id="stock_status"
+              className={fooditem.stock ? "greenColor" : "redColor"}
+            >
+              {fooditem.stock ? "In Stock" : "Out of Stock"}
+            </span>
+          </p>
         </div>
-
+      </div>
     </div>
-      
-    
   );
 }
