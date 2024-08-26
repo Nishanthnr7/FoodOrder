6f41c4562a5c6aa361088337acbaf44776efe563
Nishanthnr7 <127499581+Nishanthnr7@users.noreply.github.com>
2024-08-26 14:19:34 +0530

    minor change

---
+++ frontend/src/src/App.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect } from "react";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/layouts/Header";
+import Home from "./components/layouts/Home";
+import Footer from "./components/layouts/Footer";
+import Menu from "./components/layouts/Menu";
+import Register from "./components/users/Register";
+import Login from "./components/users/Login";
+import store from "./store";
+import Profile from "./components/users/Profile";
+import UpdateProfile from "./components/users/UpdateProfile";
+import ForgotPassword from "./components/users/ForgotPassword";
+import NewPassword from "./components/users/NewPassword"
+import { loadUser } from "./actions/userAction";
+
+export default function App() {
+  //this 3 lines are for not logging out whwn reloaded
+  //dispatched exactly once when the component is rendered and check if the user is authenticated or not
+  console.log(store);
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <div className="container container-fluid">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/eats/stores/:id/menus" element={<Menu />} />
+            <Route path="/users/signup" element={<Register />} />
+            <Route path="/users/login" element={<Login />} />
+            <Route path="/users/me" element={<Profile />} />
+            <Route path="/users/me/update" element={<UpdateProfile />} />
+            <Route path="/users/forgotPassword" element={<ForgotPassword />} />
+            <Route
+              path="/users/resetPassword/:token"
+              element={<NewPassword />}
+            />
+          </Routes>
+        </div>
+        <Footer />
+      </div>
+    </BrowserRouter>
+  );
+}
---
+++ frontend/src/src/components/layouts/CountRestaurant.jsx
@@ -0,0 +1,43 @@
+import React, { useEffect } from "react";
+import { getRestaurants } from "../../actions/restaurantAction";
+import { useDispatch, useSelector } from "react-redux";
+
+export default function CountRestaurant() {
+  const dispatch = useDispatch();
+  const { loading, error, count, showVegOnly, pureVegRestaurantCount } = useSelector((state) => state.restaurants);
+
+  useEffect(() => {
+    dispatch(getRestaurants());
+  }, [dispatch]);
+
+  // Debugging the values before rendering
+  // console.log('showVegOnly:', showVegOnly);
+  // console.log('pureVegRestaurantCount:', pureVegRestaurantCount);
+  // console.log('count:', count);
+
+  return (
+    <div>
+      {loading ? (
+        <p>Loading Restaurant count...</p>
+      ) : error ? (
+        <p>Error: {error}</p>
+      ) : (
+        <>
+          <p className="NumOfRestro">
+            {showVegOnly ? pureVegRestaurantCount : count}{" "}
+            <span className="Restro">
+              {showVegOnly
+                ? pureVegRestaurantCount === 1
+                  ? "Restaurant"
+                  : "Restaurants"
+                : count === 1
+                ? "Restaurant"
+                : "Restaurants"}
+            </span>
+          </p>
+          <hr />
+        </>
+      )}
+    </div>
+  );
+}
